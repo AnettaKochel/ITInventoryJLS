2540042c6e55895ca9c68df0a662db38ab647f82
JLS User <you@example.com>
2026-02-04 23:38:59 -0800

    Initial commit for JLSInventoryApp (cleaned config, added .gitignore)

--- Migrations/20260124103000_DropDBPasswordFromDBUser.cs
+++ Migrations/20260124103000_DropDBPasswordFromDBUser.cs
@@ -9,9 +9,16 @@ namespace ITInventoryJLS.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "DBPassword",
-                table: "DBUsers");
+            migrationBuilder.Sql(@"IF COL_LENGTH('DBUsers','DBPassword') IS NOT NULL
+    BEGIN
+        DECLARE @var0 sysname;
+        SELECT @var0 = [d].[name]
+        FROM [sys].[default_constraints] [d]
+        INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+        WHERE ([d].[parent_object_id] = OBJECT_ID(N'[DBUsers]') AND [c].[name] = N'DBPassword');
+        IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [DBUsers] DROP CONSTRAINT [' + @var0 + ']');
+        ALTER TABLE [DBUsers] DROP COLUMN [DBPassword];
+    END");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
